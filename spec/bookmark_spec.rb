bdac2e52fe3a8ad19c6a58921dfa13fc6ee48342
Daniel Ridgway <ridginald@gmail.com>
2018-06-05 14:49:23 +0100

    step09 - before refactor

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -19,4 +19,13 @@ describe Bookmark do
       expect(Bookmark.all).to eq expected_bookmarks
     end
   end
+
+  describe '.create' do
+  it 'creates a new bookmark' do
+    Bookmark.create(url: 'http://www.testbookmark.com')
+
+    expect(Bookmark.all).to include 'http://www.testbookmark.com'
+  end
+end
+
 end
