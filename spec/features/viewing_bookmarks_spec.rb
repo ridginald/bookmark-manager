f482574a852a504b9858a93de4820b58f2baf8d2
Daniel Ridgway <ridginald@gmail.com>
2018-06-05 11:19:16 +0100

    User Stort 1

---
+++ spec/features/viewing_bookmarks_spec.rb
@@ -0,0 +1,9 @@
+# feature 'viewing bookmarks' do
+#   scenario 'a user can see bookmarks' do
+#     visit('/bookmarks')
+#
+#     expect(page).to have_content "http://makersacademy.com"
+#     expect(page).to have_content "http://destroyallsoftware.com"
+#     expect(page).to have_content "http://google.com"
+#   end
+# end
